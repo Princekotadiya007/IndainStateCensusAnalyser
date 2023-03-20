121fed752740141f8b901f8f9bf9e20a02f32687
Princekotadiya007 <princuuu77@gmail.com>
2023-03-20 20:52:16 +0530

    [prince] load the indian state census information csv file

---
+++ IndianStateCensusAnalyser/IndianStateCensusAnalyser/CsvStateCensus.cs
@@ -0,0 +1,29 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyser
+{
+    public class CsvStateCensus
+    {
+        public void ReadStatecensusData(string filePath)
+        {
+            using(var render = new StreamReader(filePath))
+            {
+                using (var csv = new CsvReader(render, CultureInfo.InvariantCulture))
+                {
+                    var records = csv.GetRecords<StateCensusAnalyserModel>().ToList();
+                    foreach (var record in records)
+                    {
+                        Console.WriteLine($"{record.State} {record.Population} {record.AreaInSqKm} {record.DensityPerSqKm}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ IndianStateCensusAnalyser/IndianStateCensusAnalyser/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyser
+{
+    public class Program
+    {
+        public static string filePath = @"D:\RFP-demo\IndainStateCensusAnalyser\IndianStateCensusAnalyser\IndianStateCensusAnalyser\File\StateCensusData.csv";
+        static void Main(string[] args)
+        {
+            CsvStateCensus csvState = new CsvStateCensus();
+            csvState.ReadStatecensusData(filePath);
+        }
+    }
+}
