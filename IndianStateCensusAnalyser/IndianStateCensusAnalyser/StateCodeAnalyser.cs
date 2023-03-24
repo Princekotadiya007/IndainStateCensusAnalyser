84bd6e74c4ec2b4bcc99de52ce0ee481127cd54c
Princekotadiya007 <princuuu77@gmail.com>
2023-03-24 17:40:58 +0530

    [prince] ensure number of record matchs for state code

---
+++ IndianStateCensusAnalyser/IndianStateCensusAnalyser/StateCodeAnalyser.cs
@@ -0,0 +1,31 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyser
+{
+    public class StateCodeAnalyser
+    {
+        public int ReadStateCodeData(string filePath)
+        {
+            using (var reader = new StreamReader(filePath))
+            {
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    var records = csv.GetRecords<StateCodeModel>().ToList();
+                    foreach (var record in records)
+                    {
+                        //Console.WriteLine($"{record.SrNo} {record.Name} {record.TIN} {record.StateCode}");
+                        Console.WriteLine(record);
+                    }
+                    return records.Count() - 1;
+                }
+            }
+        }
+    }
+}
---
+++ IndianStateCensusAnlyserTest/StateCodeTest.cs
@@ -0,0 +1,24 @@
+﻿using IndianStateCensusAnalyser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnlyserTest
+{
+    public class StateCodeTest
+    {
+        public static string stateCodePath = @"D:\RFP-demo\IndainStateCensusAnalyser\IndianStateCensusAnalyser\IndianStateCensusAnalyser\File\StateCode.csv";
+        public static string incorrectStatePath = @"D:\RFP-demo\IndainStateCensusAnalyser\IndianStateCensusAnalyser\IndianStateCensusAnalyser\File\StateCode.csv";
+
+        StateCodeAnalyser stateAnalyserCode = new StateCodeAnalyser();
+        CsvStateCode csvStateCode = new CsvStateCode();
+
+        [Test]
+        public void GivenStateCodeData_ShouldMatchNumbersOfRetrunMatches()
+        {
+            Assert.AreEqual(stateAnalyserCode.ReadStateCodeData(stateCodePath), csvStateCode.ReadStateCodeData(stateCodePath));
+        }
+    }
+}
