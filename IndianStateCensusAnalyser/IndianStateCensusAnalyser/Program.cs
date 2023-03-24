521e99f0168406cecc0b29698233a4ee74048c08
Princekotadiya007 <princuuu77@gmail.com>
2023-03-24 17:22:08 +0530

    [prince] create load state code data

--- IndianStateCensusAnalyser/IndianStateCensusAnalyser/Program.cs
+++ IndianStateCensusAnalyser/IndianStateCensusAnalyser/Program.cs
@@ -9,10 +9,16 @@ namespace IndianStateCensusAnalyser
     public class Program
     {
         public static string filePath = @"D:\RFP-demo\IndainStateCensusAnalyser\IndianStateCensusAnalyser\IndianStateCensusAnalyser\File\StateCensusData.csv";
+        public static string filePath1 = @"D:\RFP-demo\IndainStateCensusAnalyser\IndianStateCensusAnalyser\IndianStateCensusAnalyser\File\StateCode.csv";
         static void Main(string[] args)
         {
             CsvStateCensus csvState = new CsvStateCensus();
             csvState.ReadStateCensusData(filePath);
+
+            CsvStateCode csvStateCode = new CsvStateCode();
+            csvStateCode.ReadStateCodeData(filePath1);
+
+            Console.ReadLine();
         }
     }
 }
